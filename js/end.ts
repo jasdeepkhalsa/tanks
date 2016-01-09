b1cf9e60d60db2eda2703dab3fb9be26f345174e
Jasdeep Khalsa <jasdeep@simplyspiritdesign.com>
2016-01-09 13:33:00 +0000

    Adding textStyle method to calculate styles

--- js/end.ts
+++ js/end.ts
@@ -2,13 +2,25 @@
 
 class End {
     private text;
+    private subtext;
 
     constructor(private game) {}
     preload() {}
+    textStyle(size = 64, colour = '#FFF') {
+        return { font: size+'px Monaco, Ubuntu Mono, Menlo, Consolas, monospace', fill: colour }
+    }
     create() {
+        // Reset game size & camera
+        this.game.world.removeAll();
+        this.game.world.setBounds(0, 0, 800, 600);
+        this.game.camera.visible = false;
+
+        // Decorate the screen
         this.game.stage.backgroundColor = "#4488AA";
-        this.text = this.game.add.text(this.game.world.width / 2, this.game.world.height / 2, 'Game Over', { font: '64px Monaco, Ubuntu Mono, Menlo, Consolas, monospace', fill: '#FFF' });
+        this.text = this.game.add.text(this.game.world.width / 2, this.game.world.height / 2, 'Game Over', this.textStyle());
+        this.subtext = this.game.add.text(this.game.world.width / 2, (this.game.world.height / 2) + 50, 'Tap to Play Again', this.textStyle(32));
         this.text.anchor.setTo(0.5, 0.5);
+        this.subtext.anchor.setTo(0.5, 0.5);
     }
     update() {
         this.game.input.onTap.add(() => {
--- js/intro.ts
+++ js/intro.ts
@@ -7,9 +7,12 @@ class Intro {
     preload() {
         this.game.load.pack('start', 'assets/pack.json');
     }
+    textStyle(size = 64, colour = '#FFF') {
+        return { font: size+'px Monaco, Ubuntu Mono, Menlo, Consolas, monospace', fill: colour }
+    }
     create() {
         this.game.stage.backgroundColor = "#4488AA";
-        this.text = this.game.add.text(this.game.world.width / 2, this.game.world.height / 2, 'Tap to Start', { font: '64px Monaco, Ubuntu Mono, Menlo, Consolas, monospace', fill: '#FFF' });
+        this.text = this.game.add.text(this.game.world.width / 2, this.game.world.height / 2, 'Tap to Start', this.textStyle());
         this.text.anchor.setTo(0.5, 0.5);
     }
     update() {
